c97f79580db48aff930cc031b5f157f6401564f0
Rachel <borapark13@gmail.com>
2022-05-18 23:42:52 +0900

    Add: useAxios

---
+++ useAxios/App.js
@@ -0,0 +1,15 @@
+import useAxios from "./useAxios";
+
+export default function App() {
+  const { loading, error, data, refetch } = useAxios({
+    url: "https://yts.torrentbay.to/api/v2/movie_details.json",
+  });
+
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading ? "loading" : "loading"}</h2>
+      <button onClick={refetch}>Refetch Button</button>
+    </div>
+  );
+}
---
+++ useAxios/useAxios.js
@@ -0,0 +1,40 @@
+import defaultAxios from "axios";
+import { useState, useEffect } from "react";
+
+const useAxios = (opts, axiosInstance = defaultAxios) => {
+  //instance를 가지고 있지 않다면 기본값으로 defaultAxios를 세팅한다
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+
+  const [trigger, setTrigger] = useState(0);
+
+  if (!opts.url) {
+    return;
+  }
+
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now());
+  };
+
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({ ...state, loading: false, data });
+        data.setHeader("Access-Control-Allow-origin", opts.url);
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+
+  return { ...state, refetch };
+};
+
+export default useAxios;
